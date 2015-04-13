a6560dc9458b1d979751368d600e0454976bcaa5
Dave Potts <dave@goopot.co.uk>
2015-04-13 21:44:51 +0100

    init

---
+++ SpecflowTutorial/OrderingAnswersSteps.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpecflowTutorial
+{
+    [Binding]
+    public class OrderingAnswersSteps {
+
+    
+
+        private QuestionModel q;
+    
+        [Given(@"There is a question ""(.*)"" with the answers")]
+        public void GivenThereIsAQuestionWithTheAnswers(string p0, Table table)
+        {
+            q = new QuestionModel();
+            q.question = p0;
+            foreach (var row in table.Rows) 
+            {
+                q.answers.Add(row["Answer"], Int32.Parse(row["Vote"]));
+            }
+            
+    
+        }
+
+        [When(@"you upvote the answer ""(.*)""")]
+public void WhenYouUpvoteTheAnswer(string p0)
+{
+    q.upvote(p0);
+}
+
+        [Then(@"the answer ""(.*)"" should be on top")]
+public void ThenTheAnswerShouldBeOnTop(string p0)
+{
+    Assert.AreEqual("Cucumber Green", q.TopAnswer());
+}
+    }
+
+
+    public class QuestionModel
+    {
+        public string question { get; set; }
+        public Dictionary<string, int> answers { get; set; }
+
+        public QuestionModel()
+        {
+            answers = new Dictionary<string, int>();
+        }
+
+        public void upvote(string answer)
+        {
+            answers[answer] += 1;
+        }
+
+        public string TopAnswer()
+        {
+            var sortedAnswers = from pair in answers
+                                orderby pair.Value descending
+                                select pair;
+            return sortedAnswers.First().Key;
+        }
+    }
+
+    
+
+}
---
+++ SpecflowTutorial/Tutorial.feature
@@ -0,0 +1,9 @@
+﻿Feature: Ordering answers
+
+Scenario: The answer with the highest vote gets to the top
+	Given There is a question "What is your favourite colour?" with the answers
+		| Answer         | Vote |
+		| Red            | 1    |
+		| Cucumber Green | 1    |
+	When you upvote the answer "Cucumber Green"
+	Then the answer "Cucumber Green" should be on top
